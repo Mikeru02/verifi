be19b3e55af499fdbaf35916916ae0f3a4c732c9
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-09-20 01:15:16 +0800

    Added trigrams

--- models/models.py
+++ models/models.py
@@ -5,16 +5,17 @@ from modules.utils.tupler import Tupler
 from modules.core.trainer import TrainNaiveBayes
 
 class TuplesConverter(Command):
-    def __init__(self, model_file_path: str, csv_file_path: str) -> None:
+    def __init__(self, model_file_path: str, csv_file_path: str, typeoftupple="content") -> None:
         self.__model_file_path = Path(model_file_path)
         self.__csv_file_path = csv_file_path
-    
+        self.__typeoftupple = typeoftupple
+        
     def execute(self) -> list:
         if self.__model_file_path.exists():
             model = Open_File(self.__model_file_path, mode="rb").execute()
             return model
         else:
-            data = Tupler(self.__csv_file_path).execute()
+            data = Tupler(self.__csv_file_path, self.__typeoftupple).execute()
             Save_File(self.__model_file_path, data, mode="wb").execute()
             return data
         
@@ -28,12 +29,15 @@ class NaiveBayes(Command):
             model = Open_File(self.__model_file_path, mode="rb").execute()
             return model
         else:
-            class_count, class_word_count, vocab_size = TrainNaiveBayes(self.__document_data, include_stopwords=True).execute()
+            model = TrainNaiveBayes(self.__document_data, include_stopwords=True).execute()
 
             model_data = {
-                "class_count": class_count,
-                "class_word_count": class_word_count,
-                "vocab_size": vocab_size
+                "class_count": model["class_count"],
+                "class_word_count": model["class_word_count"],
+                "vocab_size": model["vocab_size"],
+                "unigrams": model["unigrams"],
+                "bigrams": model["bigrams"],
+                "trigrams": model["trigrams"]
             }
 
             Save_File(self.__model_file_path, model_data, mode="wb").execute()
